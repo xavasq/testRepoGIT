1337ad32a62d35c7f7aaffc7fe3a1408d84827bd
vazorian <vazoriandev@gmail.com>
2025-03-05 16:46:02 +0500

    коммит с гитигнором

---
+++ folder/third.go
@@ -0,0 +1,7 @@
+package folder
+
+import "fmt"
+
+func SayHello() {
+	fmt.Println("приввеееет")
+}
---
+++ second.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("привет")
+	fmt.Println("второй привет")
+}
